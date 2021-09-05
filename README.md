ea09aee7a566f6ec4bffa1b29314fb2b84997642
ajaysharma12799 <ajaysharma12799@gmail.com>
2021-09-05 10:51:28 +0530

    Initial Project Setup

---
+++ README.md
@@ -0,0 +1,1 @@
+## React UnSplash Image Gallery
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      Working App
+    </div>
+  )
+}
+
+export default App
